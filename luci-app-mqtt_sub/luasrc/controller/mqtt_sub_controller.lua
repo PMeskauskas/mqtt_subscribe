d4226e318b03e3cce7a41bd69ff6c3a1aa35bdb3
PMeskauskas <paulius134679@gmail.com>
2021-09-08 10:05:00 +0300

    fixed config parsing

--- luci-app-mqtt_sub/luasrc/controller/mqtt_sub_controller.lua
+++ luci-app-mqtt_sub/luasrc/controller/mqtt_sub_controller.lua
@@ -2,5 +2,4 @@ module("luci.controller.mqtt_sub_controller", package.seeall)
 
 function index()
     entry( { "admin", "services", "mqtt", "subscriber" }, cbi("mqtt_sub"), _("Subscriber"), 100).leaf = true
-    entry( { "admin", "services", "mqtt", "publisher" }, cbi("mqtt_pub"), _("Publisher"), 50).leaf = true
 end
--- mqtt_sub/src/mqtt_sub.c
+++ mqtt_sub/src/mqtt_sub.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <syslog.h>
-
+#include <uci.h>
 volatile int interrupt = 0;
 
 void sigHandler(int signo) {
@@ -21,7 +21,7 @@ void cleanup(){
 }
 
 void usage(void) {
-    syslog(LOG_ERR, "Usage: remoteAddress port topicToSubscribe QoS ... ...");
+    syslog(LOG_ERR, "Usage: remoteAddress port ");
     cleanup();
 }
 
@@ -45,16 +45,15 @@ void mqtt_connect(struct mosquitto **mosq, char *address, int port)
 	}
 }
 
-void mqtt_subscribe(struct mosquitto **mosq, int argc, char* argv[]){
+void mqtt_subscribe(struct mosquitto **mosq, char* topicName, int qos)
+{
 	int rc = MOSQ_ERR_SUCCESS;
-	for (int i = 4; i < argc; i=i+2)
-	{
-		rc = mosquitto_subscribe(*mosq, NULL, argv[i-1], atoi(argv[i]));
-		if(rc != MOSQ_ERR_SUCCESS) {
-			syslog(LOG_ERR, "Error subscribing: %s\n", mosquitto_strerror(rc));
-			mosquitto_disconnect(*mosq);
-		}
+	rc = mosquitto_subscribe(*mosq, NULL, topicName, qos);
+	if(rc != MOSQ_ERR_SUCCESS) {
+		syslog(LOG_ERR, "Error subscribing: %s\n", mosquitto_strerror(rc));
+		mosquitto_disconnect(*mosq);
 	}
+	syslog(LOG_INFO, "subscribed to a topic: %s, QoS: %d", topicName, qos);
 }
 
 void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg)
@@ -62,33 +61,72 @@ void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_messag
 	sleep(1);
 	syslog(LOG_INFO,"Message received: %s %d %s\n", msg->topic, msg->qos, (char *)msg->payload);
 }
-
+void uci_load_package(struct uci_context *ctx, const char *config_name, struct uci_package **package)
+{
+    if(uci_load(ctx, config_name, package) !=UCI_OK){
+		syslog(LOG_ERR, "Failed to load uci");
+		uci_free_context(ctx);
+		cleanup();
+	}
+}
+void uci_element_subscribe(struct uci_package *package, struct mosquitto **mosq)
+{
+    struct uci_element *i, *j;
+    uci_foreach_element(&package->sections, i)
+	{
+		struct uci_section *section = uci_to_section(i);
+        char *section_type = section->type;
+        bool topicExists = false;
+        bool qosExists = false;
+        char *topicName = NULL;
+        int qos;
+		if(strcmp(section_type,"topic")==0){
+            uci_foreach_element(&section->options, j)
+            {
+                struct uci_option *option = uci_to_option(j);
+                char *option_name = option->e.name;
+                if (!atoi(option->v.string)){
+                    topicExists = true;
+                    topicName = option->v.string;
+                }
+                if (qos = atoi(option->v.string)){
+                   qosExists = true;
+                }
+                if(topicExists && qosExists)
+                    mqtt_subscribe(mosq, topicName, qos);
+            }
+		}
+	}
+}
 
 int main(int argc, char *argv[]) 
 {
+	const char *config_name = "mqtt_sub";
     struct mosquitto *mosq;
+	struct uci_context *ctx = uci_alloc_context();
+	struct uci_package *package;
 	int rc;
-
     openlog(NULL, LOG_CONS, LOG_USER);
 
     signal(SIGINT, sigHandler);
     signal(SIGTERM, sigHandler);
-
+	uci_load_package(ctx, config_name, &package);
+	
 	mosquitto_lib_init();
-
 	mqtt_new(&mosq);
 
 	mosquitto_message_callback_set(mosq, on_message);
-	if(argc < 4)
+
+	if(argc != 3)
 		usage();
 
 	mqtt_connect(&mosq, argv[1], atoi(argv[2]));
 
-	mqtt_subscribe(&mosq, argc, argv);
+	uci_element_subscribe(package,&mosq);
 
 	mosquitto_loop_forever(mosq, -1, 1);
-
+	
+	uci_free_context(ctx);
 	cleanup();
-
 	return 0;
 }
